87e3804dcaca8e04ac6904cd9ae49bc0cc8ac497
Joyta-Choudhury <cjoyta@gmail.com>
2017-04-18 16:16:10 -0400

    Add files via upload

---
+++ linreg-gd.py
@@ -0,0 +1,103 @@
+# linreg-gd.py
+# jchoudh1@uncc.edu
+# Joyta Choudhury
+# 
+# TODO: Write this.
+# 
+# Takes the yx file as input, where on each line y is the first element 
+# and the remaining elements constitute the x.
+
+
+import sys
+import numpy as np
+from pyspark import SparkContext
+
+
+def computex(xvalue):
+	xvalue[0]=1.0
+	return xvalue
+
+if __name__ == "__main__":
+  if len(sys.argv) !=4:
+    print >> sys.stderr, "Usage: linreg-gd.py <datafile> <alpha> <# of iterations>"
+    exit(-1)
+
+  sc = SparkContext(appName="GradientDescent")
+
+  # Input yx file has y_i as the first element of each line 
+  # and the remaining elements constitute x_i
+  yxinputFile = sc.textFile(sys.argv[1])
+
+  yxlines = yxinputFile.map(lambda line: line.split(','))
+  yxfirstline = yxlines.first()
+  yxlength = len(yxfirstline)
+  #print "yxlength: ", yxlength
+  
+  #compute y and x values using map function
+  yval = yxlines.map(lambda line: line[0]).collect()
+  xval = yxlines.map(lambda line: computex(line)).collect()
+
+  #values of y represented as matrix
+  Y = np.asmatrix(yval, dtype=float)
+  YT = Y.T				#transpose of Y matrix
+
+  #values of x represented as matrix
+  X = np.asmatrix(xval, dtype=float)
+  XT = X.T				#transpose of X matrix
+  #print Y
+  #print X
+
+  # dummy floating point array for beta to illustrate desired output format
+  beta = np.zeros(yxlength, dtype=float)
+
+  #get value of alpha and # of iterations from command line arguments
+  alpha = float(sys.argv[2])
+  iterations = int(sys.argv[3])
+
+  #Calculate the shape of beta based on size of X
+  beta_order = X.shape[1] 
+  #beta_val = np.ones_like(X).astype(float)
+
+  #beta initialized with values of 1
+  beta_val = [1 for i in range(0,beta_order)]
+  beta_iterate = []
+
+  #converge flag to check the status if beta reached convergence
+  converge_status = False
+    
+  #iterating the computations for # of iterations passed as an argument
+  for x in range(0,iterations):
+      if x == 0:
+          beta_prev = beta_val
+            
+      #computation using gradient descent formula (beta + xtranspose.alpha.(Y-X.beta))            
+      beta = np.matrix(beta_prev).T
+      xbeta = np.dot(X,np.matrix(beta)) 
+      yxbeta = np.subtract(YT,xbeta)
+      xt_yxbeta = np.dot(XT,yxbeta)
+      error = np.multiply(xt_yxbeta,alpha)
+      finalbeta = np.add(beta,error)
+       
+      #initialized a list to append all the final beta values to beta_update
+      beta_update = [] 
+      for val in finalbeta.tolist():
+          beta_update.append(val[0])
+        
+      #converge flag is set true if previous betas are equal to updated betas
+      if beta_prev == beta_update:
+          converge_status = True
+          print('Convergence at '+ str(x) +' th position')
+            
+          print ("beta: ")
+          for coeff in beta_update:       
+              print (coeff)
+          break
+      #updating values of beta for each iteration
+      beta_prev = beta_update
+      beta_iterate = beta_update
+    
+  #if converge flag is false then:
+  if not converge_status:
+      print('No covergence till : ' + str(iterations)) 
+
+  sc.stop()
---
+++ linreg.py
@@ -0,0 +1,74 @@
+# linreg.py
+# jchoudh1@uncc.edu
+# Joyta Choudhury
+# 
+# TODO: Write this.
+# 
+# Takes the yx file as input, where on each line y is the first element 
+# and the remaining elements constitute the x.
+
+
+import sys
+import numpy as np
+
+from pyspark import SparkContext
+from numpy.linalg import inv
+
+#function to compute values of a with the help of x values
+def computea(xvalues):
+	xmatrix = np.matrix(xvalues,dtype=float)	#forms a row matrix
+	xtranspose = np.insert(xmatrix, 0, 1, axis=1)	#insert 1 at 0 index
+	x = xtranspose.T				#transpose of a matrix which will be x matrix
+	mula = np.dot(x,xtranspose)			#multiply x and x transpose to get value of a
+	return mula
+	
+#function to compute values of b with the help of x values and y value
+def computeb(yvalue,xvalues):
+	xmatrix = np.matrix(xvalues,dtype=float)	#forms a row matrix
+	xtranspose = np.insert(xmatrix, 0, 1, axis=1)	#insert 1 at 0 index
+	x = xtranspose.T				#transpose of a matrix which will be x matrix
+	ymatrix = np.matrix(yvalue,dtype=float)		#forms a matrix for y
+	mulb = np.dot(x,ymatrix)			#multiply x and y matrxix to get value of b
+	return mulb
+
+if __name__ == "__main__":
+  if len(sys.argv) !=2:
+    print >> sys.stderr, "Usage: linreg <datafile>"
+    exit(-1)
+
+  sc = SparkContext(appName="LinearRegression")
+
+  # Input yx file has y_i as the first element of each line 
+  # and the remaining elements constitute x_i
+  yxinputFile = sc.textFile(sys.argv[1])
+
+  yxlines = yxinputFile.map(lambda line: line.split(','))
+  yxfirstline = yxlines.first()
+  yxlength = len(yxfirstline)
+  #print "yxlength: ", yxlength
+
+  # dummy floating point array for beta to illustrate desired output format
+  beta = np.zeros(yxlength, dtype=float)
+  
+  #Using map and reduce to get values of a and b
+  valuesa = yxlines.map(lambda line: computea(line[1:])).reduce(lambda x,y: np.add(x,y))
+  valuesb = yxlines.map(lambda line: computeb(line[0],line[1:])).reduce(lambda x,y: np.add(x,y))
+ 
+  #print valuesa
+  #print valuesb
+
+  #taking inverse of a
+  inversea = inv(np.matrix(valuesa))
+  
+  #computing values of beta by multiplying inverse of a with b
+  beta = np.dot(inversea,valuesb)
+  
+  #to save as an output file
+  #np.savetxt('yxlin1-s.out',beta)
+
+  # print the linear regression coefficients in desired output format
+  print "beta: "
+  for coeff in beta:
+      print coeff
+
+  sc.stop()
